8d4fa1aecafc077f4c16e1d2a62dcf6d0e1730a6
Angelyn Pham <angelynpham26@gmail.com>
2024-04-20 15:22:47 -0400

    added calendar and add event

---
+++ foodie_app/app/add_event.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const add_event = () => {
+  return (
+    <View>
+      <Text>add new event</Text>
+    </View>
+  )
+}
+
+export default add_event
+
+const styles = StyleSheet.create({})
