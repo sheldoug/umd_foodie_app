6c1b09a43d11efaee04dd87d99eebc959321f1ca
Samar Khan <shamchee1110@gmail.com>
2024-04-20 21:14:45 -0400

    added map functionality

--- foodie_app/app/map.jsx
+++ foodie_app/app/map.jsx
@@ -1,14 +1,33 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import MapView, {PROVIDER_GOOGLE, UrlTile} from 'react-native-maps'
 
 const map = () => {
   return (
-    <View>
-      <Text>map</Text>
+    <View style={styles.container}>
+      <MapView 
+        style={styles.map} 
+        initialRegion={{
+          latitude: 38.986172,
+          longitude: -76.942362,
+          latitudeDelta: 0.00922,
+          longitudeDelta: 0.00421,
+        }}>
+          <UrlTile urlTemplate='http://c.tile.openstreetmap.org/{z}/{x}/{y}.png'/>
+      </MapView>
     </View>
   )
 }
 
 export default map
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  map : {
+    ...StyleSheet.absoluteFillObject
+  }
+})
