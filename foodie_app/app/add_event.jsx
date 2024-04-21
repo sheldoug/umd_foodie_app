15cafa6bb87700004cd3f4672d10cfa3565bdf64
Sheldon <sheldoug13@icloud.com>
2024-04-20 21:33:52 -0400

    add ScrollView

--- foodie_app/app/add_event.jsx
+++ foodie_app/app/add_event.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, TextInput, Button, Text } from 'react-native';
+import { StyleSheet, View, TextInput, Button, Text, ScrollView} from 'react-native';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
@@ -57,60 +57,64 @@ const AddEvent = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Add Food Event</Text>
-      <Text style={styles.label}>Event Name</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter the name of the event"
-        value={eventName}
-        onChangeText={setEventName}
-      />
-      <Text style={styles.label}>Date</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="YYYY-MM-DD"
-        value={eventDate}
-        onChangeText={setEventDate}
-      />
-      <Text style={styles.label}>Start Time</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="HH:MM"
-        value={eventStartTime}
-        onChangeText={setEventStartTime}
-      />
-      <Text style={styles.label}>End Time</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="HH:MM"
-        value={eventEndTime}
-        onChangeText={setEventEndTime}
-      />
-      <Text style={styles.label}>Location/Building</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter event location/building"
-        value={eventLocation}
-        onChangeText={setEventLocation}
-      />
-      <Text style={styles.label}>Room Number (Optional)</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter event room number (optional)"
-        value={eventRoomNumber}
-        onChangeText={setEventRoomNumber}
-      />
-      <Text style={styles.label}>Description</Text>
-      <TextInput
-        style={[styles.input, styles.textArea]}
-        placeholder="Enter description of the event"
-        value={eventDescription}
-        onChangeText={setEventDescription}
-        multiline
-      />
-      <Button title="Add Event" onPress={handleAddEvent} />
-    </View>
+    <ScrollView>
+
+
+      <View style={styles.container}>
+        <Text style={styles.title}>Add Food Event</Text>
+        <Text style={styles.label}>Event Name</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter the name of the event"
+          value={eventName}
+          onChangeText={setEventName}
+        />
+        <Text style={styles.label}>Date</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="YYYY-MM-DD"
+          value={eventDate}
+          onChangeText={setEventDate}
+        />
+        <Text style={styles.label}>Start Time</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="HH:MM"
+          value={eventStartTime}
+          onChangeText={setEventStartTime}
+        />
+        <Text style={styles.label}>End Time</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="HH:MM"
+          value={eventEndTime}
+          onChangeText={setEventEndTime}
+        />
+        <Text style={styles.label}>Location/Building</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter event location/building"
+          value={eventLocation}
+          onChangeText={setEventLocation}
+        />
+        <Text style={styles.label}>Room Number (Optional)</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter event room number (optional)"
+          value={eventRoomNumber}
+          onChangeText={setEventRoomNumber}
+        />
+        <Text style={styles.label}>Description</Text>
+        <TextInput
+          style={[styles.input, styles.textArea]}
+          placeholder="Enter description of the event"
+          value={eventDescription}
+          onChangeText={setEventDescription}
+          multiline
+        />
+        <Button title="Add Event" onPress={handleAddEvent} />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -120,6 +124,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 20,
+    backgroundColor: "#FFF8F0"
   },
   title: {
     fontSize: 24,
@@ -130,16 +135,17 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginBottom: 5,
     alignSelf: 'flex-start',
+    fontFamily: "Inter_900Black"
   },
   input: {
     height: 40,
     width: '100%',
     borderColor: 'black',
-    borderWidth: 1,
+    borderWidth: 2,
     marginBottom: 20,
     paddingHorizontal: 10,
     fontSize: 16,
-    opacity: 0.7,
+    opacity: 1,
   },
   textArea: {
     height: 100,
