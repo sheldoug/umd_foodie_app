470aa88a65ca4f3dc589a850e2fe01854f171a78
Samar Khan <shamchee1110@gmail.com>
2024-04-21 00:51:25 -0400

    added marker for events on map

--- foodie_app/app/add_event.jsx
+++ foodie_app/app/add_event.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, TextInput, Button, Text } from 'react-native';
+import { StyleSheet, View, TextInput, Button, Text, ScrollView} from 'react-native';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
@@ -24,13 +24,15 @@ const AddEvent = () => {
   const [eventDescription, setEventDescription] = useState('');
   const [eventLocation, setEventLocation] = useState('');
   const [eventRoomNumber, setEventRoomNumber] = useState('');
+  const [latitude, setLatitude] = useState('0.0'); // Default latitude
+  const [longitude, setLongitude] = useState('0.0'); // Default longitude
 
   const handleAddEvent = async () => {
     if (!eventName || !eventDate || !eventStartTime || !eventEndTime || !eventDescription || !eventLocation) {
-        alert('Please fill in all required fields');
-        return;
-      }
-      
+      alert('Please fill in all required fields');
+      return;
+    }
+
     try {
       await addDoc(collection(firestore, 'events'), {
         eventName: eventName,
@@ -39,7 +41,9 @@ const AddEvent = () => {
         eventEndTime: eventEndTime,
         eventDescription: eventDescription,
         eventLocation: eventLocation,
-        eventRoomNumber: eventRoomNumber
+        eventRoomNumber: eventRoomNumber,
+        latitude: latitude, // Include latitude in the document
+        longitude: longitude // Include longitude in the document
       });
       // Reset form fields
       setEventName('');
@@ -49,6 +53,8 @@ const AddEvent = () => {
       setEventDescription('');
       setEventLocation('');
       setEventRoomNumber('');
+      setLatitude(''); // Reset latitude
+      setLongitude(''); // Reset longitude
     } catch (error) {
       console.error('Error adding event: ', error);
       // Show error message
@@ -57,60 +63,78 @@ const AddEvent = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Add Food Event</Text>
-      <Text style={styles.label}>Event Name</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter the name of the event"
-        value={eventName}
-        onChangeText={setEventName}
-      />
-      <Text style={styles.label}>Date</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="YYYY-MM-DD"
-        value={eventDate}
-        onChangeText={setEventDate}
-      />
-      <Text style={styles.label}>Start Time</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="HH:MM"
-        value={eventStartTime}
-        onChangeText={setEventStartTime}
-      />
-      <Text style={styles.label}>End Time</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="HH:MM"
-        value={eventEndTime}
-        onChangeText={setEventEndTime}
-      />
-      <Text style={styles.label}>Location/Building</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter event location/building"
-        value={eventLocation}
-        onChangeText={setEventLocation}
-      />
-      <Text style={styles.label}>Room Number (Optional)</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter event room number (optional)"
-        value={eventRoomNumber}
-        onChangeText={setEventRoomNumber}
-      />
-      <Text style={styles.label}>Description</Text>
-      <TextInput
-        style={[styles.input, styles.textArea]}
-        placeholder="Enter description of the event"
-        value={eventDescription}
-        onChangeText={setEventDescription}
-        multiline
-      />
-      <Button title="Add Event" onPress={handleAddEvent} />
-    </View>
+    <ScrollView>
+
+
+      <View style={styles.container}>
+        <Text style={styles.title}>Add Food Event</Text>
+        <Text style={styles.label}>Event Name</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter the name of the event"
+          value={eventName}
+          onChangeText={setEventName}
+        />
+        <Text style={styles.label}>Date</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="YYYY-MM-DD"
+          value={eventDate}
+          onChangeText={setEventDate}
+        />
+        <Text style={styles.label}>Start Time</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="HH:MM"
+          value={eventStartTime}
+          onChangeText={setEventStartTime}
+        />
+        <Text style={styles.label}>End Time</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="HH:MM"
+          value={eventEndTime}
+          onChangeText={setEventEndTime}
+        />
+        <Text style={styles.label}>Location/Building</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter event location/building"
+          value={eventLocation}
+          onChangeText={setEventLocation}
+        />
+        <Text style={styles.label}>Room Number (Optional)</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter event room number (optional)"
+          value={eventRoomNumber}
+          onChangeText={setEventRoomNumber}
+        />
+        <Text style={styles.label}>Description</Text>
+        <TextInput
+          style={[styles.input, styles.textArea]}
+          placeholder="Enter description of the event"
+          value={eventDescription}
+          onChangeText={setEventDescription}
+          multiline
+        />
+        <Text style={styles.label}>Latitude</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Latitude"
+          value={latitude}
+          editable={false} // Make the field read-only
+        />
+        <Text style={styles.label}>Longitude</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Longitude"
+          value={longitude}
+          editable={false} // Make the field read-only
+        />
+        <Button title="Add Event" onPress={handleAddEvent} />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -120,6 +144,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 20,
+    backgroundColor: "#FFF8F0"
   },
   title: {
     fontSize: 24,
@@ -130,16 +155,17 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginBottom: 5,
     alignSelf: 'flex-start',
+    fontFamily: "Inter_900Black"
   },
   input: {
     height: 40,
     width: '100%',
     borderColor: 'black',
-    borderWidth: 1,
+    borderWidth: 2,
     marginBottom: 20,
     paddingHorizontal: 10,
     fontSize: 16,
-    opacity: 0.7,
+    opacity: 1,
   },
   textArea: {
     height: 100,
